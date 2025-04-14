31506c454411b7bb4545262c866a90fb083cd840
GokuMohandas <gokumd@gmail.com>
2025-04-14 02:29:13 -0700

    added precommit

--- clear_cell_nums.py
+++ clear_cell_nums.py
@@ -9,15 +9,14 @@ def clear_execution_numbers(nb_path):
     for cell in nb["cells"]:
         if cell["cell_type"] == "code":
             cell["execution_count"] = None
-            for output in cell["outputs"]:
+            for output in cell.get("outputs", []):
                 if "execution_count" in output:
                     output["execution_count"] = None
     with open(nb_path, "w", encoding="utf-8") as f:
         nbformat.write(nb, f)
 
 
-if __name__ == "__main__":
-    NOTEBOOK_DIR = Path(__file__).parent
-    notebook_fps = list(NOTEBOOK_DIR.glob("**/*.ipynb"))
-    for fp in notebook_fps:
-        clear_execution_numbers(fp)
+NOTEBOOK_DIR = Path(".")
+notebook_fps = list(NOTEBOOK_DIR.glob("**/*.ipynb"))
+for fp in notebook_fps:
+    clear_execution_numbers(fp)
